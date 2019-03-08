b408e62d15a694c78054f4047589de934a09b99b
DragstylerNL <dragstylernl@gmail.com>
2019-03-08 21:13:36 +0100

    Unity is setup, pr.Camera- get mouse pos) added

---
+++ CODENAME_StrikeForce/Assets/_Scripts/FollowMyMousePointer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMyMousePointer : MonoBehaviour
+{
+
+    public Camera PlayerCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get the position of the mouse
+        Vector3 mousePos = Input.mousePosition;
+        // solve math problem beforhand by setting the ofset of the camera
+        mousePos.z = 10;
+        // calculate and position the object to the position of the mouse in the actual "world"
+        this.transform.position = PlayerCam.ScreenToWorldPoint(mousePos);
+    }
+
+}
